951f8465c839300aa25e87cf32c351cf7186623f
dipondeb <developerdipon@gmail.com>
2024-12-11 22:30:45 +0600

    add timer logic & complete profile screen

--- lib/features/auth/ui/screens/otp_verification_screen.dart
+++ lib/features/auth/ui/screens/otp_verification_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
 import 'package:crafty_bay_ecommerce_app/app/app_colors.dart';
+import 'package:crafty_bay_ecommerce_app/app/app_constant.dart';
+import 'package:crafty_bay_ecommerce_app/features/auth/ui/screens/complete_profile_screen.dart';
 import 'package:crafty_bay_ecommerce_app/features/auth/ui/widgets/app_logo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
 class OtpVerificationScreen extends StatefulWidget {
@@ -16,6 +22,27 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
 
   final TextEditingController _otpTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final RxInt _remainingTime = AppConstant.resendOtpTimeOutInSecs.obs;
+  late Timer timer;
+  final RxBool _enableResendCodeButton = false.obs;
+
+  @override
+  void initState() {
+    super.initState();
+    _startResendCodeTimer();
+  }
+
+  void _startResendCodeTimer() {
+    _enableResendCodeButton.value = false;
+    _remainingTime.value = AppConstant.resendOtpTimeOutInSecs;
+    timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      _remainingTime.value--;
+      if (_remainingTime.value == 0) {
+        t.cancel();
+        _enableResendCodeButton.value = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,55 +51,31 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(24),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const SizedBox(height: 80),
-                const AppLogoWidget(),
-                const SizedBox(height: 16),
-                Text(
-                  'Enter OTP Code',
-                  style: textTheme.titleLarge,
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              const AppLogoWidget(),
+              const SizedBox(height: 16),
+              Text(
+                'Enter OTP Code',
+                style: textTheme.titleLarge,
+              ),
+              Text(
+                'A 4 digit OTP code has been sent',
+                style: textTheme.bodyLarge?.copyWith(
+                  color: Colors.grey,
                 ),
-                Text(
-                  'A 4 digit OTP code has been sent',
-                  style: textTheme.bodyLarge?.copyWith(
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                _buildPinCodeTextField(),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // if (_formKey.currentState!.validate()) {}
-                  },
-                  child: const Text('Next'),
-                ),
-                const SizedBox(height: 24),
-                RichText(
-                  text: const TextSpan(
-                      text: 'This code will be expire in ',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: '120s',
-                        style: TextStyle(
-                          color: AppColors.themeColor,
-                        ),
-                      ),
-                    ]
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Resend Code'),
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16),
+              _buildPinCodeTextField(),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _onTapNextButton,
+                child: const Text('Next'),
+              ),
+              const SizedBox(height: 24),
+              _buildTimerSection(),
+            ],
           ),
         ),
       ),
@@ -80,36 +83,84 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
   }
 
   Widget _buildPinCodeTextField() {
-    return PinCodeTextField(
-      controller: _otpTEController,
-      appContext: context,
-      keyboardType: TextInputType.number,
-      length: 6,
-      obscureText: false,
-      animationType: AnimationType.fade,
-      animationDuration: const Duration(milliseconds: 300),
-      enableActiveFill: true,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      backgroundColor: Colors.transparent,
-      textStyle: const TextStyle(
-        color: Colors.black,
-      ),
-      pinTheme: PinTheme(
-        shape: PinCodeFieldShape.box,
-        selectedColor: AppColors.themeColor,
-        activeColor: AppColors.themeColor,
-        inactiveColor: AppColors.themeColor,
-        activeFillColor: Colors.white,
-        inactiveFillColor: Colors.transparent,
-        selectedFillColor: Colors.white,
-        borderRadius: BorderRadius.circular(8),
+    return Form(
+      key: _formKey,
+      child: PinCodeTextField(
+        controller: _otpTEController,
+        appContext: context,
+        keyboardType: TextInputType.number,
+        length: 6,
+        obscureText: false,
+        animationType: AnimationType.fade,
+        animationDuration: const Duration(milliseconds: 300),
+        enableActiveFill: true,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        backgroundColor: Colors.transparent,
+        textStyle: const TextStyle(
+          color: Colors.black,
+        ),
+        pinTheme: PinTheme(
+          shape: PinCodeFieldShape.box,
+          selectedColor: AppColors.themeColor,
+          activeColor: AppColors.themeColor,
+          inactiveColor: AppColors.themeColor,
+          activeFillColor: Colors.white,
+          inactiveFillColor: Colors.transparent,
+          selectedFillColor: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+        ),
       ),
     );
+  }
+
+  Widget _buildTimerSection() {
+    return Column(
+      children: [
+        Obx(
+          () => Visibility(
+            visible: !_enableResendCodeButton.value,
+            child: RichText(
+              text: TextSpan(
+                text: 'This code will be expire in ',
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+                children: [
+                  TextSpan(
+                    text: '${_remainingTime}s',
+                    style: const TextStyle(
+                      color: AppColors.themeColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Obx(
+          () => Visibility(
+            visible: _enableResendCodeButton.value,
+            child: TextButton(
+              onPressed: () {
+                _startResendCodeTimer();
+              },
+              child: const Text('Resend Code'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onTapNextButton() {
+    // if (_formKey.currentState!.validate()) {}
+    Navigator.pushNamed(context, CompleteProfileScreen.name);
   }
 
   @override
   void dispose() {
     _otpTEController.dispose();
+    timer.cancel();
     super.dispose();
   }
 }
