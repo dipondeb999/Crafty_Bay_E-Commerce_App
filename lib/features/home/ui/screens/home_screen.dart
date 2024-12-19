94f548d3166901d2f230216107c72dad721ca306
dipondeb <developerdipon@gmail.com>
2024-12-19 21:20:51 +0600

    add home screen

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:crafty_bay_ecommerce_app/app/assets_path.dart';
+import 'package:crafty_bay_ecommerce_app/features/home/ui/widgets/app_bar_icon_button.dart';
+import 'package:crafty_bay_ecommerce_app/features/home/ui/widgets/home_carousel_slider.dart';
+import 'package:crafty_bay_ecommerce_app/features/home/ui/widgets/product_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,12 +15,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchBarController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('home'),
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              ProductSearchBar(
+                controller: _searchBarController,
+              ),
+              const SizedBox(height: 16),
+              const HomeCarouselSlider(),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetsPath.navBarAppLogoSvg),
+      actions: [
+        AppBarIconButton(
+          onTap: () {},
+          icon: Icons.person_outline,
+        ),
+        const SizedBox(width: 8),
+        AppBarIconButton(
+          onTap: () {},
+          icon: Icons.call_outlined,
+        ),
+        const SizedBox(width: 8),
+        AppBarIconButton(
+          onTap: () {},
+          icon: Icons.notifications_outlined,
+        ),
+        const SizedBox(width: 8),
+      ],
+    );
+  }
 }
+
---
+++ lib/features/home/ui/widgets/home_carousel_slider.dart
@@ -0,0 +1,76 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:crafty_bay_ecommerce_app/app/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeCarouselSlider extends StatefulWidget {
+  const HomeCarouselSlider({super.key,});
+
+  @override
+  State<HomeCarouselSlider> createState() => _HomeCarouselSliderState();
+}
+
+class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
+  final ValueNotifier<int> _selectedIndex = ValueNotifier(0);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 180,
+            viewportFraction: 0.95,
+            onPageChanged: (currentIndex, reason) {
+              _selectedIndex.value = currentIndex;
+            },
+          ),
+          items: [1, 2, 3, 4, 5].map(
+            (i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 2),
+                    decoration: BoxDecoration(
+                        color: AppColors.themeColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'text $i',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ).toList(),
+        ),
+        const SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < 5; i++)
+            ValueListenableBuilder(
+              valueListenable: _selectedIndex,
+              builder: (context, value, _) {
+                return Container(
+                  height: 15,
+                  width: 15,
+                  margin: const EdgeInsets.symmetric(horizontal: 2),
+                  decoration: BoxDecoration(
+                    color: value == i ? AppColors.themeColor : Colors.transparent,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: AppColors.themeColor,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
