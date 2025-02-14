6425aee61c22365040a5b0da71f0ee859fa92687
dipondeb <developerdipon@gmail.com>
2025-02-15 00:10:39 +0600

    update auth model

--- lib/features/auth/data/models/profile_model.dart
+++ lib/features/auth/data/models/profile_model.dart
@@ -1,52 +1,96 @@
+import 'package:crafty_bay_ecommerce_project/features/auth/data/models/user_model.dart';
+
 class ProfileModel {
-  String? sId;
-  String? firstName;
-  String? lastName;
-  String? email;
-  bool? emailVerified;
-  String? phone;
-  bool? phoneVerified;
-  Null avatarUrl;
-  String? city;
-  int? role;
+  int? id;
+  String? cusName;
+  String? cusAdd;
+  String? cusCity;
+  String? cusState;
+  String? cusPostcode;
+  String? cusCountry;
+  String? cusPhone;
+  String? cusFax;
+  String? shipName;
+  String? shipAdd;
+  String? shipCity;
+  String? shipState;
+  String? shipPostcode;
+  String? shipCountry;
+  String? shipPhone;
+  int? userId;
+  String? createdAt;
+  String? updatedAt;
+  UserModel? user;
 
   ProfileModel(
-      {this.sId,
-        this.firstName,
-        this.lastName,
-        this.email,
-        this.emailVerified,
-        this.phone,
-        this.phoneVerified,
-        this.avatarUrl,
-        this.city,
-        this.role});
+      {this.id,
+        this.cusName,
+        this.cusAdd,
+        this.cusCity,
+        this.cusState,
+        this.cusPostcode,
+        this.cusCountry,
+        this.cusPhone,
+        this.cusFax,
+        this.shipName,
+        this.shipAdd,
+        this.shipCity,
+        this.shipState,
+        this.shipPostcode,
+        this.shipCountry,
+        this.shipPhone,
+        this.userId,
+        this.createdAt,
+        this.updatedAt,
+        this.user});
 
   ProfileModel.fromJson(Map<String, dynamic> json) {
-    sId = json['_id'];
-    firstName = json['first_name'];
-    lastName = json['last_name'];
-    email = json['email'];
-    emailVerified = json['email_verified'];
-    phone = json['phone'];
-    phoneVerified = json['phone_verified'];
-    avatarUrl = json['avatar_url'];
-    city = json['city'];
-    role = json['role'];
+    id = json['id'];
+    cusName = json['cus_name'];
+    cusAdd = json['cus_add'];
+    cusCity = json['cus_city'];
+    cusState = json['cus_state'];
+    cusPostcode = json['cus_postcode'];
+    cusCountry = json['cus_country'];
+    cusPhone = json['cus_phone'];
+    cusFax = json['cus_fax'];
+    shipName = json['ship_name'];
+    shipAdd = json['ship_add'];
+    shipCity = json['ship_city'];
+    shipState = json['ship_state'];
+    shipPostcode = json['ship_postcode'];
+    shipCountry = json['ship_country'];
+    shipPhone = json['ship_phone'];
+    userId = json['user_id'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    user = json['user'] != null ? UserModel.fromJson(json['user']) : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['_id'] = sId;
-    data['first_name'] = firstName;
-    data['last_name'] = lastName;
-    data['email'] = email;
-    data['email_verified'] = emailVerified;
-    data['phone'] = phone;
-    data['phone_verified'] = phoneVerified;
-    data['avatar_url'] = avatarUrl;
-    data['city'] = city;
-    data['role'] = role;
+    data['id'] = id;
+    data['cus_name'] = cusName;
+    data['cus_add'] = cusAdd;
+    data['cus_city'] = cusCity;
+    data['cus_state'] = cusState;
+    data['cus_postcode'] = cusPostcode;
+    data['cus_country'] = cusCountry;
+    data['cus_phone'] = cusPhone;
+    data['cus_fax'] = cusFax;
+    data['ship_name'] = shipName;
+    data['ship_add'] = shipAdd;
+    data['ship_city'] = shipCity;
+    data['ship_state'] = shipState;
+    data['ship_postcode'] = shipPostcode;
+    data['ship_country'] = shipCountry;
+    data['ship_phone'] = shipPhone;
+    data['user_id'] = userId;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    if (user != null) {
+      data['user'] = user!.toJson();
+    }
     return data;
   }
 }
